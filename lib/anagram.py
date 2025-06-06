09c1fa4e0ee0ab2cf5b47e98ddde5e409259246b
biwott-v <victor.biwott@student.moringaschool.com>
2025-06-06 12:05:45 +0300

    completes assignment

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,9 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, word_list):
+        '''Returns a list of words from word_list that are anagrams of the initialized word.'''
+        # Sort the letters in the word and compare to sorted letters of each word in the list
+        return [word for word in word_list if sorted(word) == sorted(self.word)]
